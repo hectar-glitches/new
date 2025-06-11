66377ebe533665a3ab09d9f86f69d5994be4f982
Hectar <84522781+hectar-glitches@users.noreply.github.com>
2025-06-11 11:44:48 -0500

    Update use-mobile.tsx

--- hooks/use-mobile.tsx
+++ hooks/use-mobile.tsx
@@ -1,19 +1,20 @@
-import * as React from "react"
+import { useEffect, useState } from "react"
 
 const MOBILE_BREAKPOINT = 768
 
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
+  const [isMobile, setIsMobile] = useState<boolean>(false)
 
-  React.useEffect(() => {
-    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
-    const onChange = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    }
-    mql.addEventListener("change", onChange)
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    return () => mql.removeEventListener("change", onChange)
+  useEffect(() => {
+    const mediaQuery = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
+    const handleChange = (event: MediaQueryListEvent) => setIsMobile(event.matches)
+
+    // Set initial value
+    setIsMobile(mediaQuery.matches)
+
+    mediaQuery.addEventListener("change", handleChange)
+    return () => mediaQuery.removeEventListener("change", handleChange)
   }, [])
 
-  return !!isMobile
+  return isMobile
 }
